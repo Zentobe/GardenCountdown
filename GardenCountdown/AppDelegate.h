8bef69646bdf401f3a6b42ee7f00c7e862155b02
Dabid Nguyen <David@Doraimon-2.local>
2014-11-09 20:28:19 -0800

    Initial Commit

---
+++ GardenCountdown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GardenCountdown
+//
+//  Created by Dabid Nguyen on 11/9/14.
+//  Copyright (c) 2014 David Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GardenCountdown/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GardenCountdown
+//
+//  Created by Dabid Nguyen on 11/9/14.
+//  Copyright (c) 2014 David Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
